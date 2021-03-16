2cb47030554f694537f586c3ee0d06fbe148ff82
runningturtles <wangxn@gmx.de>
2021-03-16 17:19:57 +0100

    Added comments and description

--- ChildCareAPI.py
+++ ChildCareAPI.py
@@ -1,41 +1,55 @@
-# Class ChildCareAPI
+# Class ChildCareAPI provides the interfaces to
+# import, query and filter data
+
 import ChildCare as ChildCare_Class
 import re
 import os
 
 class ChildCareAPI:
+	# Initializing ChildCareAPI with empty data
 	def __init__(self):
 		self.datasets = []
 
+	# Reading data sets from a file.
+	# If the file doesn't exist, throw out an error message,
+	# otherwise generate a list of data sets
 	def read_data(self, filename):
+
+		# Checking if file exists
 		if not os.path.isfile(filename):
 			raise Exception("File doesn't exist.")
 
+		# Opening file and reading the contents line by line
 		self.filename = filename
 		fh = open(filename, 'r')
 		count = 0
 		while True:
 			count += 1 
 			# Get next line from file,
-			# if line is empty
-			# end of file is reached
+			# if line is empty, end of file is reached
 			line = fh.readline()
 			if not line:
 				break
-
+			# If the content is a year, then two data sets will be 
+			# added into the list 
 			if (re.match(r"\d{4}", line.strip())):
+				# Initializing the variables
 				cc1 = ChildCare_Class.ChildCare()
 				cc2 = ChildCare_Class.ChildCare()
 				cc1.set_category("Tageseinrichtung")
 				cc2.set_category("Tagespflege")
-
 				year = line.strip()
 				num_kids1 = []
 				num_kids2 = []
 				num_lunches1 = ""
 				num_lunches2 = ""
+				# Skipping the line about the total number of the kids.
+				# Because this information can be calculated by available data.
 				line = fh.readline()
 				line = fh.readline()
+				# The next three lines are about the number of children
+				# categorized by ages; the fourth line is about the number of
+				# children taken lunches.
 				for i in range(4):
 					sp = []
 					count += 1
@@ -48,6 +62,7 @@ class ChildCareAPI:
 					else:
 						num_lunches1 = sp[1].strip()
 						num_lunches2 = sp[2].strip()
+				# Setting values and appending the two data sets to the list
 				cc1.set_num_kids(num_kids1)
 				cc2.set_num_kids(num_kids2)
 				cc1.set_num_lunches(num_lunches1)
@@ -55,11 +70,12 @@ class ChildCareAPI:
 				cc1.set_year(year)
 				cc2.set_year(year)
 				self.datasets.append(cc1)        
-				self.datasets.append(cc2)       
+				self.datasets.append(cc2)
+		# Closing the file
 		fh.close()
 		return 0
        
-         
+    # Filtering the data sets by year     
 	def filter_by_year(self, year):
 		cc = ChildCare_Class.ChildCare()
 		res = []
