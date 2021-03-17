1a5769e3a5e57cbc75ad88a558c7428bf5cb1287
runningturtles <wangxn@gmx.de>
2021-03-17 15:12:45 +0100

    Setup file for building and publishing python package added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Child Care API',
+    version='0.0.1',
+    description='API to get information about \
+                the statistic information of \
+                the child care situation in BW',
+    url='https://github.com/runningturtles/ChildCare.git',
+    author='Running Turtles',
+    author_email='wangxn@gmx.com',
+    license='unlicense',
+    packages=['ChildCare']
+)
